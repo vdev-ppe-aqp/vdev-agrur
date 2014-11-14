d418e5f8f98f0820afac162c49727228ce944fec
philippedub91 <philippedub91.av@gmail.com>
2014-11-14 12:16:17 +0100

    Fonctions javascript
    Ajout d'un fichier de fonctions javascript

---
+++ javascript/fonction-agrur.js
@@ -0,0 +1,11 @@
+/**
+ * Checks if a text field is not empty
+ *
+ * @param {string} nomChamp The name of the field to check
+ * @return {boolean} Returns
+ */
+ function veriForm(nomChamp){
+ 	var flag;
+ 	//Traitements
+ 	return flag;
+ }
